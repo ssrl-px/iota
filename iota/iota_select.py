3588d4d4e7f45060f08c93267a0f600a38f4816a
Artem Lyubimov <alyubimov@users.noreply.github.com>
2015-01-08 03:25:43 +0000

    IOTA v0.7; more log info & reject list files

--- iota/iota_select.py
+++ iota/iota_select.py
@@ -22,39 +22,40 @@ from xfel.clustering.singleframe import SingleFrame
 def prefilter(gs_params, total_tmp_pickles):
 
     acceptable_pickles = []
-    for tmp_pickle in total_tmp_pickles:
+    if gs_params.flag_prefilter == True:
+        for tmp_pickle in total_tmp_pickles:
 
-        tmp_pickle_name = os.path.basename(tmp_pickle)
-        uc_tol = gs_params.target_uc_tolerance
-        user_uc = []
+            tmp_pickle_name = os.path.basename(tmp_pickle)
+            uc_tol = gs_params.target_uc_tolerance
+            user_uc = []
 
-        for prm in gs_params.target_unit_cell.parameters():
-            user_uc.append(prm)
+            for prm in gs_params.target_unit_cell.parameters():
+                user_uc.append(prm)
 
-        # read pickle info and determine uc differences
-        observations = SingleFrame(tmp_pickle, tmp_pickle_name).miller_array
-        p_pg = observations.space_group_info()
-        p_uc = observations.unit_cell().parameters()
+            # read pickle info and determine uc differences
+            observations = SingleFrame(tmp_pickle, tmp_pickle_name).miller_array
+            p_pg = observations.space_group_info()
+            p_uc = observations.unit_cell().parameters()
 
-        delta_a = abs(p_uc[0] - user_uc[0])
-        delta_b = abs(p_uc[1] - user_uc[1])
-        delta_c = abs(p_uc[2] - user_uc[2])
-        delta_alpha = abs(p_uc[3] - user_uc[3])
-        delta_beta = abs(p_uc[4] - user_uc[4])
-        delta_gamma = abs(p_uc[5] - user_uc[5])
+            delta_a = abs(p_uc[0] - user_uc[0])
+            delta_b = abs(p_uc[1] - user_uc[1])
+            delta_c = abs(p_uc[2] - user_uc[2])
+            delta_alpha = abs(p_uc[3] - user_uc[3])
+            delta_beta = abs(p_uc[4] - user_uc[4])
+            delta_gamma = abs(p_uc[5] - user_uc[5])
 
-        # Determine if pickle satisfies sg / uc parameters within given
-        # tolerance and low resolution cutoff
-        if str(p_pg) == gs_params.target_pointgroup:
-            if (
-                delta_a <= user_uc[0] * uc_tol
-                and delta_b <= user_uc[1] * uc_tol
-                and delta_c <= user_uc[2] * uc_tol
-                and delta_alpha <= user_uc[3] * uc_tol
-                and delta_beta <= user_uc[4] * uc_tol
-                and delta_gamma <= user_uc[5] * uc_tol
-            ):
-                acceptable_pickles.append(tmp_pickle)
+            # Determine if pickle satisfies sg / uc parameters within given
+            # tolerance and low resolution cutoff
+            if str(p_pg) == gs_params.target_pointgroup:
+                if (
+                    delta_a <= user_uc[0] * uc_tol
+                    and delta_b <= user_uc[1] * uc_tol
+                    and delta_c <= user_uc[2] * uc_tol
+                    and delta_alpha <= user_uc[3] * uc_tol
+                    and delta_beta <= user_uc[4] * uc_tol
+                    and delta_gamma <= user_uc[5] * uc_tol
+                ):
+                    acceptable_pickles.append(tmp_pickle)
 
     return acceptable_pickles
 
@@ -154,159 +155,174 @@ def best_file_selection(gs_params, output_dir, log_dir):
         ]
 
         # apply prefilter if specified and make a list of acceptable pickles
-        if gs_params.flag_prefilter == True:
+        if len(total_tmp_pickles) == 0:
+            ps_logger.info("No integrated images found " "in {}:\n".format(tmp_dir))
+            acceptable_pickles = []
+
+        else:
             acceptable_pickles = prefilter(gs_params, total_tmp_pickles)
-        else:
-            acceptable_pickles = total_tmp_pickles
-
-        # Selection and copying of pickles, output of stats to log file
-        if len(acceptable_pickles) == 0:
-            ps_logger.info(
-                "Discarded all {0} integrated pickles "
-                "in {1}:\n".format(len(acceptable_pickles), tmp_dir)
-            )
-        else:
-            ps_logger.info(
-                "Selecting from {0} out "
-                "of {1} integrated pickles"
-                "in {2}:\n".format(
-                    len(acceptable_pickles), len(total_tmp_pickles), tmp_dir
+            if len(acceptable_pickles) == 0:
+                ps_logger.info(
+                    "Discarded all {0} integrated pickles "
+                    "in {1}:\n".format(len(total_tmp_pickles), tmp_dir)
                 )
-            )
-            filename = str(os.path.split(acceptable_pickles[0])[1])
-            categories = " {:^{pwidth}}{:^16}{:^15}{:^45}" "{:^12}{:^10}".format(
-                "Filename",
-                "resolution",
-                "s.g.",
-                "unit cell",
-                "spots",
-                "strong",
-                pwidth=len(filename) + 5,
-            )
-            line = " {:-^{pwidth}}{:-^16}{:-^15}{:-^45}" "{:-^12}{:-^10}".format(
-                "", "", "", "", "", "", pwidth=len(filename) + 5
-            )
-            ps_logger.info(categories)
-            ps_logger.info(line)
 
-            # Report pickle stats. Mark selected pickle with asterisk for posterity
-            for pickle in acceptable_pickles:
-                pickle_name = os.path.split(pickle)[1]
+                with open(
+                    "{}/prefilter_fail.lst".format(os.path.abspath(gs_params.output)),
+                    "a",
+                ) as bad_int:
+                    bad_int.write("{}\n".format(tmp_dir))
 
-                observations = SingleFrame(
-                    pickle, os.path.split(pickle)[1]
-                ).miller_array
-                res = observations.d_max_min()
-                pg = observations.space_group_info()
-                uc = observations.unit_cell().parameters()
-                rl_observations = observations.resolution_filter(
-                    gs_params.selection_res_limit.d_max,
-                    gs_params.selection_res_limit.d_min,
-                )
-                I_over_sigI = rl_observations.data() / rl_observations.sigmas()
-                ref = len(rl_observations.data())
-                sref = len([val for val in I_over_sigI if val >= gs_params.min_sigma])
-
-                info_line = (
-                    "  {:<{pwidth}}{:>7.2f} - {:<5.2f}{:^15}{:>6.2f},"
-                    "{:>6.2f}, {:>6.2f}, {:>6.2f}, {:>6.2f}, "
-                    "{:>6.2f}{:^12}{:^10}".format(
-                        pickle_name,
-                        res[0],
-                        res[1],
-                        pg,
-                        uc[0],
-                        uc[1],
-                        uc[2],
-                        uc[3],
-                        uc[4],
-                        uc[5],
-                        ref,
-                        sref,
-                        pwidth=len(filename) + 5,
+            else:
+                # Selection and copying of pickles, output of stats to log file
+                ps_logger.info(
+                    "Selecting from {0} out "
+                    "of {1} integrated pickles"
+                    "in {2}:\n".format(
+                        len(acceptable_pickles), len(total_tmp_pickles), tmp_dir
                     )
                 )
-                ps_logger.info(info_line)
-
-            # Make selections & copy files
-            selected_info = []
-
-            # Total reflections
-            sel_pickle = best_by_reflections(
-                gs_params, acceptable_pickles, output_dir + "/best_by_total"
-            )
-            with open(
-                "{}/best_by_total.lst".format(os.path.abspath(gs_params.output)), "a"
-            ) as best_file:
-                best_file.write("{}\n".format(sel_pickle))
-            selected_info.append(["T", sel_pickle, os.path.split(sel_pickle)[1]])
-
-            # Strong reflections
-            sel_pickle = best_by_strong(
-                gs_params, acceptable_pickles, output_dir + "/best_by_strong"
-            )
-            with open(
-                "{}/best_by_strong.lst".format(os.path.abspath(gs_params.output)), "a"
-            ) as best_file:
-                best_file.write("{}\n".format(sel_pickle))
-            selected_info.append(["S", sel_pickle, os.path.split(sel_pickle)[1]])
-
-            # Unit cell
-            sel_pickle = best_by_uc(
-                gs_params, acceptable_pickles, output_dir + "/best_by_uc"
-            )
-            with open(
-                "{}/best_by_uc.lst".format(os.path.abspath(gs_params.output)), "a"
-            ) as best_file:
-                best_file.write("{}\n".format(sel_pickle))
-            selected_info.append(["U", sel_pickle, os.path.split(sel_pickle)[1]])
-
-            # x,y offset
-            sel_pickle = best_by_offset(
-                acceptable_pickles, output_dir + "/best_by_offset"
-            )
-            with open(
-                "{}/best_by_offset.lst".format(os.path.abspath(gs_params.output)), "a"
-            ) as best_file:
-                best_file.write("{}\n".format(sel_pickle))
-            selected_info.append(["O", sel_pickle, os.path.split(sel_pickle)[1]])
-
-            # Output selected file information
-            ps_logger.info("\nSelected:")
-            for sel in selected_info:
-                observations = SingleFrame(sel[1], sel[2]).miller_array
-                res = observations.d_max_min()
-                pg = observations.space_group_info()
-                uc = observations.unit_cell().parameters()
-
-                rl_observations = observations.resolution_filter(
-                    gs_params.selection_res_limit.d_max,
-                    gs_params.selection_res_limit.d_min,
+                filename = str(os.path.split(acceptable_pickles[0])[1])
+                categories = " {:^{pwidth}}{:^16}{:^15}{:^45}" "{:^12}{:^10}".format(
+                    "Filename",
+                    "resolution",
+                    "s.g.",
+                    "unit cell",
+                    "spots",
+                    "strong",
+                    pwidth=len(filename) + 5,
                 )
-                I_over_sigI = rl_observations.data() / rl_observations.sigmas()
-                ref = len(rl_observations.data())
-                sref = len([val for val in I_over_sigI if val >= gs_params.min_sigma])
+                line = " {:-^{pwidth}}{:-^16}{:-^15}{:-^45}" "{:-^12}{:-^10}".format(
+                    "", "", "", "", "", "", pwidth=len(filename) + 5
+                )
+                ps_logger.info(categories)
+                ps_logger.info(line)
 
-                info_line = (
-                    "{} {:<{pwidth}}{:>7.2f} - {:<5.2f}{:^15}{:>6.2f}, "
-                    "{:>6.2f}, {:>6.2f}, {:>6.2f}, {:>6.2f}, "
-                    "{:>6.2f}{:^12}{:^10}".format(
-                        sel[0],
-                        sel[2],
-                        res[0],
-                        res[1],
-                        pg,
-                        uc[0],
-                        uc[1],
-                        uc[2],
-                        uc[3],
-                        uc[4],
-                        uc[5],
-                        ref,
-                        sref,
-                        pwidth=len(filename) + 5,
+                # Report pickle stats. Mark selected pickle with asterisk for posterity
+                for pickle in acceptable_pickles:
+                    pickle_name = os.path.split(pickle)[1]
+
+                    observations = SingleFrame(
+                        pickle, os.path.split(pickle)[1]
+                    ).miller_array
+                    res = observations.d_max_min()
+                    pg = observations.space_group_info()
+                    uc = observations.unit_cell().parameters()
+                    rl_observations = observations.resolution_filter(
+                        gs_params.selection_res_limit.d_max,
+                        gs_params.selection_res_limit.d_min,
                     )
+                    I_over_sigI = rl_observations.data() / rl_observations.sigmas()
+                    ref = len(rl_observations.data())
+                    sref = len(
+                        [val for val in I_over_sigI if val >= gs_params.min_sigma]
+                    )
+
+                    info_line = (
+                        "  {:<{pwidth}}{:>7.2f} - {:<5.2f}{:^15}{:>6.2f},"
+                        "{:>6.2f}, {:>6.2f}, {:>6.2f}, {:>6.2f}, "
+                        "{:>6.2f}{:^12}{:^10}".format(
+                            pickle_name,
+                            res[0],
+                            res[1],
+                            pg,
+                            uc[0],
+                            uc[1],
+                            uc[2],
+                            uc[3],
+                            uc[4],
+                            uc[5],
+                            ref,
+                            sref,
+                            pwidth=len(filename) + 5,
+                        )
+                    )
+                    ps_logger.info(info_line)
+
+                # Make selections & copy files
+                selected_info = []
+
+                # Total reflections
+                sel_pickle = best_by_reflections(
+                    gs_params, acceptable_pickles, output_dir + "/best_by_total"
                 )
-                ps_logger.info(info_line)
+                with open(
+                    "{}/best_by_total.lst".format(os.path.abspath(gs_params.output)),
+                    "a",
+                ) as best_file:
+                    best_file.write("{}\n".format(sel_pickle))
+                selected_info.append(["T", sel_pickle, os.path.split(sel_pickle)[1]])
+
+                # Strong reflections
+                sel_pickle = best_by_strong(
+                    gs_params, acceptable_pickles, output_dir + "/best_by_strong"
+                )
+                with open(
+                    "{}/best_by_strong.lst".format(os.path.abspath(gs_params.output)),
+                    "a",
+                ) as best_file:
+                    best_file.write("{}\n".format(sel_pickle))
+                selected_info.append(["S", sel_pickle, os.path.split(sel_pickle)[1]])
+
+                # Unit cell
+                sel_pickle = best_by_uc(
+                    gs_params, acceptable_pickles, output_dir + "/best_by_uc"
+                )
+                with open(
+                    "{}/best_by_uc.lst".format(os.path.abspath(gs_params.output)), "a"
+                ) as best_file:
+                    best_file.write("{}\n".format(sel_pickle))
+                selected_info.append(["U", sel_pickle, os.path.split(sel_pickle)[1]])
+
+                # x,y offset
+                sel_pickle = best_by_offset(
+                    acceptable_pickles, output_dir + "/best_by_offset"
+                )
+                with open(
+                    "{}/best_by_offset.lst".format(os.path.abspath(gs_params.output)),
+                    "a",
+                ) as best_file:
+                    best_file.write("{}\n".format(sel_pickle))
+                selected_info.append(["O", sel_pickle, os.path.split(sel_pickle)[1]])
+
+                # Output selected file information
+                ps_logger.info("\nSelected:")
+                for sel in selected_info:
+                    observations = SingleFrame(sel[1], sel[2]).miller_array
+                    res = observations.d_max_min()
+                    pg = observations.space_group_info()
+                    uc = observations.unit_cell().parameters()
+
+                    rl_observations = observations.resolution_filter(
+                        gs_params.selection_res_limit.d_max,
+                        gs_params.selection_res_limit.d_min,
+                    )
+                    I_over_sigI = rl_observations.data() / rl_observations.sigmas()
+                    ref = len(rl_observations.data())
+                    sref = len(
+                        [val for val in I_over_sigI if val >= gs_params.min_sigma]
+                    )
+
+                    info_line = (
+                        "{} {:<{pwidth}}{:>7.2f} - {:<5.2f}{:^15}{:>6.2f}, "
+                        "{:>6.2f}, {:>6.2f}, {:>6.2f}, {:>6.2f}, "
+                        "{:>6.2f}{:^12}{:^10}".format(
+                            sel[0],
+                            sel[2],
+                            res[0],
+                            res[1],
+                            pg,
+                            uc[0],
+                            uc[1],
+                            uc[2],
+                            uc[3],
+                            uc[4],
+                            uc[5],
+                            ref,
+                            sref,
+                            pwidth=len(filename) + 5,
+                        )
+                    )
+                    ps_logger.info(info_line)
 
             ps_logger.info("\n")
