20388267ba015ed908efce828fb5d70b590c18b1
Artem Lyubimov <alyubimov@users.noreply.github.com>
2015-02-19 19:12:16 +0000

    IOTA v0.90. Cleaned up

--- iota/iota_gridsearch.py
+++ iota/iota_gridsearch.py
@@ -51,8 +51,7 @@ def integrate_one_image(mp_entry, log_dir, gs_params):
     3. gs_params: PHIL object containing grid-search and other parameters (from
                   user input file)
     """
-    from xfel.phil_preferences import load_cxi_phil
-    from xfel.cxi.display_spots import run_one_index_core
+
     from xfel.cxi.display_spots import run_one_index
 
     current_img = mp_entry[0]
@@ -155,7 +154,7 @@ def integrate_one_image(mp_entry, log_dir, gs_params):
         for item in index_log:
             index_logfile.write("{}\n".format(item))
 
-        index_logfile.write("{}\n\n".format(grid_search_output))
+        index_logfile.write("\n{}\n\n".format(grid_search_output))
 
 
 # Indexing and integration w/ grid search of spotfinding parameters
