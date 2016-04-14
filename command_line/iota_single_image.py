c561589a69ef2519a9f5d7bb2cce36421567b9e5
Artem Lyubimov <alyubimov@users.noreply.github.com>
2016-04-14 18:04:26 +0000

    IOTA v1.0.000P (Phenix version 1)
    Integration Optimization, Triage and Analysis tool for the processing of serial diffraction images.
    This is the first version intended to be included in the Phenix suite.
    
    Major Features:
    
    1. Semi-automated image pre-processing:
       a. Conversion from raw image format to image pickles
       b. Modification of image pickles (optional):
          - image cropping / padding to place beam x,y into the center of image
          - modification of beam x,y and distance parameters
          - thresholding of beamstop shadow
       c. Image "triage" (i.e. finding images that have diffraction, optional):
          - simple (i.e. using user-supplied spot-finding parameters)
          - grid search of spot-finding parameters (best pair of params used for processing)
       d. Converted / triaged image pickles saved in separate folder
    
    2. Integration optimization:
       a. Choice of cctbx.xfel or DIALS packages for image processing
       b. Grid search of spot-finding parameters (cctbx.xfel):
          - Brute force or "smart" options available; can be turned off altogether
          - Best pairwise combo of spot height / spot area selected and used for integration
          - Can also try several "signal height" values (3D grid search)
    
    3. Analysis of processed data:
       a. Visualization of processed images vs. integration predictions
       b. Hierarchical unit cell clustering
       c. Summary of processing parameters
       d. Summary of integration success
       e. Optional charts: mosaicity plot, lattice model residuals, etc.
       f. Grid search heatmap output for cctbx.xfel grid search
       g. Beam x,y diagram for diagnosing mis-indexing
       h. cctbx.xfel / DIALS logs saved for every image for advanced user's perusal
    
    Known Issues:
    
    1. Visualization of images may not work well; removing scipy dependencies
    2. Unit cell clustering currently not active; removing scipy dependencies
    3. Because of #2, median unit cell parameters returned; we are aware that this is woefully inadequate

--- command_line/iota_single_image.py
+++ command_line/iota_single_image.py
@@ -5,11 +5,11 @@ from __future__ import division
 """
 Author      : Lyubimov, A.Y.
 Created     : 10/12/2014
-Last Changed: 02/25/2016
-Description : Single-image IOTA module. Version 2.32
+Last Changed: 04/13/2016
+Description : Single-image IOTA module. Version 1.0.000P
 """
 
-iota_version = "2.32"
+iota_version = "1.0.000P"
 help_message = (
     "\n{:-^70}"
     "".format("Integration Optimization, Triage and Analysis")
@@ -24,10 +24,10 @@ in the command line.
 """
 )
 
-from prime.iota.iota_init import InitAll
-import prime.iota.iota_image as img
-import prime.iota.iota_cmd as cmd
-import prime.iota.iota_misc as misc
+from iota.components.iota_init import InitAll
+import iota.components.iota_image as img
+import iota.components.iota_cmd as cmd
+import iota.components.iota_misc as misc
 
 
 def run_one_image(image, init):
