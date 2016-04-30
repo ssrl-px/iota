7a7e5f97523c437cd5fdd55c7d8c685be2d4c1ba
Artem Lyubimov <alyubimov@users.noreply.github.com>
2016-04-30 01:55:42 +0000

    IOTA v1.0.003 - GUI additions & bug fixes
    New Features
    1. Better error handling (i.e. actual error handling in a few cases)
    2. GUI prompts for generating default cctbx / DIALS settings if none are given
    3. Image import, pre-processing and integration now happens in one step, per image (GUI only)
    4. Progress tracked / charts updated via timed polling rather than at process callback
    5. New charts updated in real-time:
        a. beam center coordinate chart
        b. summary pie chart (keeps track of images that failed triage, weren't integrated, etc.)
    
    Known Issues:
    1. Still a little dodgy when running IOTA with DIALS; will be improved in future
    2. More visuals in the works.
    3. Unit cell clustering currently not active; removing scipy dependencies
    4. Because of this, median unit cell parameters returned; we are aware that this is woefully inadequate
    5. Sometimes (rarely) hangs when importing a script and changing processing options - trying to reproduce
    6. Odd behavior of status bar when Abort button is pressed

--- command_line/iota_bulletproof.py
+++ command_line/iota_bulletproof.py
@@ -5,7 +5,7 @@ from __future__ import division
 """
 Author      : Brewster, A.S., Lyubimov, A.Y.
 Created     : 10/12/2014
-Last Changed: 11/06/2015
+Last Changed: 04/29/2016
 Description : Script for indexing using run_one_index_core. Designed to be
               called by easy_run.fully_buffered, protecting the calling program
               from boost errors.
@@ -35,6 +35,5 @@ if __name__ == "__main__":
             error_message = "{}: {}".format(e.classname, e[0].replace("\n", " ")[:50])
         else:
             error_message = "{}".format(str(e).replace("\n", " ")[:50])
-        print e
         # save the error message to be picked up by iota
         easy_pickle.dump(tmppath, error_message)
