3e2e7d869a2af4b0bf828d1c26394963e8d1e333
Artem Lyubimov <lyubimov@stanford.edu>
2019-12-05 15:50:43 -0800

    input entry bugfix

--- src/iota/command_line/iota_single_image.py
+++ src/iota/command_line/iota_single_image.py
@@ -87,7 +87,7 @@ class SingleImageProcessor(ProcessingBase):
                 break
 
             if os.path.isfile(img):
-                input_entry = (1, img)
+                input_entry = (1, img, 0)
                 img_object = self.import_and_process(input_entry)
 
                 n_spots = img_object.final["spots"]
@@ -123,7 +123,7 @@ class SingleImageProcessor(ProcessingBase):
 
                 if errors:
                     for e in errors:
-                        print(e)
+                        print("SINGLE IMAGE ERROR: ", e)
 
             # info = [self.index, len(observed), self.img, sg, uc]
 
