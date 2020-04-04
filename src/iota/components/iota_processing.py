7589583e9970f5c1e4ed671ca2a0066725d09fa8
Artem Lyubimov <lyubimov@stanford.edu>
2020-04-03 17:10:53 -0700

    fixed bug where processing with defaults fails

--- src/iota/components/iota_processing.py
+++ src/iota/components/iota_processing.py
@@ -156,7 +156,13 @@ class IOTAImageProcessor(Processor):
                 tphil_string = tf.read()
             tparams = phil_scope.fetch(source=parse(tphil_string)).extract()
         else:
-            tparams = phil_scope.extract()
+            from iota.components.iota_input import write_defaults
+
+            method = self.iparams.advanced.processing_backend
+            target_phil, _ = write_defaults(
+                method=method, write_param_file=False, write_target_file=False
+            )
+            tparams = phil_scope.fetch(source=target_phil).extract()
         Processor.__init__(self, params=tparams)
 
         # IOTA-specific settings from here
@@ -374,12 +380,12 @@ class IOTAImageProcessor(Processor):
         if hasattr(error, "classname"):
             # print(error.classname, "for {}:".format(img_object.img_path), )
             error_message = "{}: {}".format(
-                error.classname, error[0].replace("\n", " ")[:50]
-            )
+                error.classname, error[0].replace("\n", " ")
+            )  # [:50]
         else:
             p_name = p_name.lower().capitalize()
             # print("{} error for {}:".format(p_name, img_object.img_path), )
-            error_message = "{}".format(str(error).replace("\n", " ")[:50])
+            error_message = "{}".format(str(error).replace("\n", " "))  # [:50]
         # print(error_message)
         img_object.fail = "failed {}".format(p_name.lower())
         img_object.errors.append(error_message)
@@ -504,7 +510,7 @@ class IOTAImageProcessor(Processor):
             )
 
         # Finish if spotfinding is the last processing stage
-        if "spotfind" in self.last_stage:
+        if "spotfind" in self.last_stage or "spf" in self.last_stage:
             try:
                 detector = img_object.experiments.unique_detectors()[0]
                 beam = img_object.experiments.unique_beams()[0]
@@ -586,6 +592,9 @@ class IOTAImageProcessor(Processor):
                         )
                 else:
                     return self.error_handler(e_ridx, "indexing", img_object, output)
+
+        if "index" in self.last_stage or "idx" in self.last_stage:
+            return img_object
 
         # **** REFINEMENT **** #
         with util.Capturing() as output:
