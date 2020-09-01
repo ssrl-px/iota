190868b00c12d073b43187e9827e145d9e556095
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2020-09-01 21:36:40 +0100

    Bump version: 1.4.019 → 1.4.20

--- src/iota/__init__.py
+++ src/iota/__init__.py
@@ -9,7 +9,7 @@ Description : IOTA initialization module (also contains app info)
 
 from datetime import datetime
 
-iota_version = "1.4.019"
+iota_version = "1.4.20"
 intx_version = "1.0.001"
 now = "{:%A, %b %d, %Y. %I:%M %p}".format(datetime.now())
 
