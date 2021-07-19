5cf77eacf1fbcb99535af38aef73ad285825e4a2
Artem Lyubimov <lyubimov@stanford.edu>
2021-07-19 13:50:33 -0700

    Bump version: 1.4.21 → 1.4.22

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     long_description_content_type="text/x-rst",
     author="Leland Stanford Junior University",
     author_email="scientificsoftware@diamond.ac.uk",
-    version="1.4.21",
+    version="1.4.22",
     url="https://github.com/ssrl-px/iota",
     download_url="https://github.com/ssrl-px/iota/releases",
     license="BSD",
